63cd8411bca8fa7cc0cd04be34df1628d3deac94
Tedd OKANO <okano.akifumi@gmail.com>
2024-03-08 14:56:11 +0900

    DigitalOut works except A0, A1 as expected. and A5 <-- why?

--- source/main.cpp
+++ source/main.cpp
@@ -9,63 +9,63 @@
 #include	"r01lib.h"
 
 #include	"config.h"
-#include	"pin_control.h"
-
 r01lib_start;	/* *** place this word before making instance of r01lib classes *** */
-
-I3C			i3c;
-P3T1755		p3t1755( i3c );
 
 DigitalOut	r(    RED   );	//	== D5 pin
 DigitalOut	g(    GREEN );	//	== D6 pin
 DigitalOut	b(    BLUE  );	//	"BLUE" (D3) is a dummy LED difinition. This pin is overriden by PWM output
 DigitalOut	trig( D2    );	//	IBI detection trigger. Pin D0~D2, D4~D13, D18, D19 and A0~A5 can be used
 
-void	DAA_set_dynamic_ddress_from_static_ddress( uint8_t static_address, uint8_t dynamic_address );
+DigitalOut	dp[]	= {
+		DigitalOut( D0 ),
+		DigitalOut( D1 ),
+		DigitalOut( D2 ),
+		DigitalOut( D3 ),
+		DigitalOut( D4 ),
+		DigitalOut( D5 ),
+		DigitalOut( D6 ),
+		DigitalOut( D7 ),
+		DigitalOut( D8 ),
+		DigitalOut( D9 ),
+		DigitalOut( D10 ),
+		DigitalOut( D11 ),
+		DigitalOut( D12 ),
+		DigitalOut( D13 ),
+		DigitalOut( D18 ),
+		DigitalOut( D19 )
+};
+DigitalOut	ap[]	= {
+		DigitalOut( A0 ),
+		DigitalOut( A1 ),
+		DigitalOut( A2 ),
+		DigitalOut( A3 ),
+		DigitalOut( A4 ),
+		DigitalOut( A5 )
+};
 
 int main(void)
 {
-	init_pin_control();
-	i3c.set_IBI_callback( ibi_trigger_output );
-
-	PRINTF("\r\nP3T1755 (Temperature sensor) I3C operation sample: getting temperature data and IBI\r\n");
-
-	DAA_set_dynamic_ddress_from_static_ddress( P3T1755_ADDR_I2C, P3T1755_ADDR_I3C );
-	p3t1755.address_overwrite( P3T1755_ADDR_I3C );
-	
-	float ref_temp	= p3t1755.temp();
-	float low		= ref_temp + 1.0;
-	float high		= ref_temp + 2.0;
-
-	p3t1755.high( high );
-	p3t1755.low(  low  );
-
-	PRINTF( "  T_LOW / T_HIGH registers updated: %8.4f˚C / %8.4f˚C\r\n", low, high );
-	PRINTF( "      based on current temperature: %8.4f˚C\r\n", ref_temp );
-
-	p3t1755.conf( p3t1755.conf() | 0x02 );		//	ALART pin configured to INT mode
-	p3t1755.ccc_set( CCC::DIRECT_ENEC, 0x01 );	// Enable IBI
-
-	p3t1755.info();
-
-	float	temp;
-	uint8_t	ibi_addr;
+	r	= 1;
+	g	= 1;
+	b	= 1;
 
 	while ( true )
 	{
-		if ( (ibi_addr	= i3c.check_IBI()) )
-			PRINTF("*** IBI : Got IBI from target_address: 7’h%02X (0x%02X)\r\n", ibi_addr, ibi_addr << 1 );
-
-		temp	= p3t1755;
-		PRINTF( "Temperature: %8.4f˚C\r\n", temp );
-
-		led_set_color( temp, ref_temp );
-		wait( 1 );
+		for ( int i = 0; i < sizeof( dp ) / sizeof( DigitalOut ); i++ )
+		{
+			for ( int t = 0; t < i + 1; t++ )
+			{
+				dp[ i ]	= 1;
+				dp[ i ]	= 0;
+			}
+		}
+		for ( int i = 0; i < sizeof( ap ) / sizeof( DigitalOut ); i++ )
+		{
+			for ( int t = 0; t < i + 1; t++ )
+			{
+				ap[ i ]	= 1;
+				ap[ i ]	= 0;
+			}
+		}
 	}
-}
-
-void DAA_set_dynamic_ddress_from_static_ddress( uint8_t static_address, uint8_t dynamic_address )
-{
-	i3c.ccc_broadcast( CCC::BROADCAST_RSTDAA, NULL, 0 ); // Reset DAA
-	i3c.ccc_set( CCC::DIRECT_SETDASA, static_address, dynamic_address << 1 ); // Set Dynamic Address from Static Address
 }
